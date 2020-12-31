c1a439581b6ce493f8e60b46dfb83f9ba8cb671e
Kerem <kerem0comert@gmail.com>
2020-12-31 12:35:19 +0300

    Initial commit

---
+++ DiagramDesigner/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace DiagramDesigner
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            tbFederateName.Text = "adfads ";
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void bAddFederate_Click(object sender, RoutedEventArgs e)
+        {
+
+            string federateName = tbFederateName.Text;
+            string federateDesc = tbFederateDesc.Text;
+           
+       
+
+        }
+    }
+}
